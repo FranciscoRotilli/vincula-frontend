696c14cc3c7460ea2fede5a546b82e9afe5fddb4
Francisco Rotilli <francisco.r@edu.pucrs.br>
2025-08-22 17:14:01 -0300

    feat(input): added additional exemples to showcase the input component

--- src/components/input/InputShowcase.tsx
+++ src/components/input/InputShowcase.tsx
@@ -2,37 +2,114 @@
 import React from 'react'
 
 import Input from './Input'
-import { CalendarMonth, DateRange, Lock, Person } from '@mui/icons-material'
+import { CalendarMonth, DateRange, Lock, Person, Search } from '@mui/icons-material'
 
 const InputShowcase = () => {
 
     const [value, setValue] = React.useState("")
 
     return (
-        <div style={{ background: "#fff", padding: 20 }}>
-            <Input 
-                // For number, use text + validate with regex /^\d+$/
-                onChange = {e => setValue(e.target.value)} // Optional, function
-                value = {value} // Optional, string
-                name = "Name" // Optional, string
-                id = "id-input" // Optional, string
-                variant = 'outlined' // Optional (default: outlined), 'outlined' | 'filled' | 'standard'
-                //height = {40} // Optional (default: 40), number
-                type = "text" // Optional (defalut: "text"), 'text' | 'password' | 'email' | 'tel' | 'date' | 'time' | 'datetime-local' | 'search'
-                //required = {true} // Optional (default: false), boolean
-                placeholder = "Placeholder" // Mandatory, string
-                //disabled = {false} // Optional (default: false), boolean
-                label = "Label" // Optional, string
-                //error = "Error message" // Optional, string
-                //validate={value => {
-                //    if (!/^\d+$/.test(value)) return "Apenas números são permitidos"
-                //    return undefined
-                //}} // Optional, function
-                //startIcon = {<CalendarMonth />} // Optional, icon element https://mui.com/material-ui/material-icons/
-                //endIcon = {<CalendarMonth />} // // Optional, icon element
-                //minLength={10} // Optional, number
-                //maxLength={128} // Optional, number
-            />
+        <div style={{ 
+            display: 'flex',
+            flexWrap: 'wrap',
+            gap: '10px',
+            padding: '20px',
+            backgroundColor: '#ffffff'
+        }}>
+            <div style={{ background: "#fff", padding: 20, width: 350, flexShrink: 0 }}>
+                <Input 
+                    // For number, use text + validate with regex /^\d+$/
+                    onChange = {e => setValue(e.target.value)} // Optional, function
+                    value = {value} // Optional, string
+                    name = "Name" // Optional, string
+                    id = "id-input" // Optional, string
+                    variant = 'outlined' // Optional (default: outlined), 'outlined' | 'filled' | 'standard'
+                    //height = {40} // Optional (default: 40), number
+                    type = "text" // Optional (defalut: "text"), 'text' | 'password' | 'email' | 'tel' | 'date' | 'time' | 'datetime-local' | 'search'
+                    //required = {true} // Optional (default: false), boolean
+                    placeholder = "Placeholder" // Mandatory, string
+                    //disabled = {false} // Optional (default: false), boolean
+                    label = "Label" // Optional, string
+                    //error = "Error message" // Optional, string
+                    //validate={value => {
+                    //    if (!/^\d+$/.test(value)) return "Apenas números são permitidos"
+                    //    return undefined
+                    //}} // Optional, function
+                    //startIcon = {<CalendarMonth />} // Optional, icon element https://mui.com/material-ui/material-icons/
+                    //endIcon = {<CalendarMonth />} // // Optional, icon element
+                    //minLength={10} // Optional, number
+                    //maxLength={128} // Optional, number
+                />
+            </div>
+            <div style={{ background: "#fff", padding: 20, width: 350, flexShrink: 0 }}>
+                <Input 
+                    placeholder = "Placeholder"
+                    label = "Label"
+                    error = "Error message"
+                />
+            </div>
+            <div style={{ background: "#fff", padding: 20, width: 350, flexShrink: 0 }}>
+                <Input 
+                    placeholder = "Placeholder"
+                    label = "Label"
+                    error = " "
+                    startIcon={<Search />}
+                />
+            </div>
+            <div style={{ background: "#fff", padding: 20, width: 350, flexShrink: 0 }}>
+                <Input 
+                    placeholder = "Insira o usuário"
+                    label = "Usuário"
+                    required
+                    startIcon={<Person />}
+                />
+            </div>
+            <div style={{ background: "#fff", padding: 20, width: 350, flexShrink: 0 }}>
+                <Input 
+                    placeholder = "Insira a senha"
+                    label = "Senha"
+                    required
+                    type='password'
+                    startIcon={<Person />}
+                />
+            </div>
+            <div style={{ background: "#fff", padding: 20, width: 350, flexShrink: 0 }}>
+                <Input 
+                    placeholder = "Insira o usuário"
+                    label = "Usuário"
+                    error="Usuário ou senha inválido."
+                    required
+                    startIcon={<Person />}
+                />
+            </div>
+            <div style={{ background: "#fff", padding: 20, width: 350, flexShrink: 0 }}>
+                <Input 
+                    placeholder = "Insira a senha"
+                    label = "Senha"
+                    error="Usuário ou senha inválido."
+                    required
+                    type='password'
+                    startIcon={<Person />}
+                />
+            </div>
+            <div style={{ background: "#fff", padding: 20, width: 350, flexShrink: 0 }}>
+                <Input 
+                    variant="standard"
+                    placeholder = "Insira o usuário"
+                    label = "Usuário"
+                    required
+                    startIcon={<Person />}
+                />
+            </div>
+            <div style={{ background: "#fff", padding: 20, width: 350, flexShrink: 0 }}>
+                <Input 
+                    variant="filled"
+                    placeholder = "Insira a senha"
+                    label = "Senha"
+                    required
+                    startIcon={<Person />}
+                />
+            </div>
         </div>
     )
 }
