e5361ab6cf6946ffb586804c1e19812c57574a5b
Nícolas Vargas <nicolas.v02@edu.pucrs.br>
2025-08-26 13:30:14 -0300

    Implementar e testar componente footer

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Image from "next/image";
+import styles from "./Footer.module.css";
+
+export default function Footer() {
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.left}>
+        <p className={styles.text}>
+          {currentYear} © Ministério Público do Estado do Rio Grande do Sul
+        </p>
+      </div>
+
+      <div className={styles.right}>
+        <Image
+            src="/vincula.png"
+            alt="Vincula"
+            width={140}
+            height={22}
+          />
+          <Image
+            src="/logo.png"
+            alt="Logomarca Vincula"
+            width={54}
+            height={54}
+          />
+      </div>
+    </footer>
+  );
+}
