71396ff757cba2c5870331c8d90a28bdbb984af4
Roberto <roberto.neto@roxcode.dev>
2025-08-26 18:02:54 -0300

    feat(tests): add unit tests for Button component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -46,7 +46,7 @@ export default function Home() {
           size="small"
           icon={<FiAperture />}
 				/> */}
-        <ButtonShowcase />{' '}
+        {/*<ButtonShowcase />}
         {/* Showcase com variações do componente botão, 
 				use <Button /> para testar o botão indivudalmente */}
       </main>
