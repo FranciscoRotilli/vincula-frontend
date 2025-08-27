e1f06660843a0661df1ef53bd8ab061defd1e734
Nícolas Vargas <nicolas.v02@edu.pucrs.br>
2025-08-27 15:25:25 -0300

    Adicionar teste de imagens.

--- test/components/footer/Footer.test.tsx
+++ test/components/footer/Footer.test.tsx
@@ -11,4 +11,10 @@ describe('Footer Component', () => {
       screen.getByText(/Ministério Público do Estado do Rio Grande do Sul/i)
     ).toBeInTheDocument();
   });
+
+  it('renders footer images', () => {
+    render(<Footer />);
+    const image = screen.getAllByAltText(/vincula/i);
+    expect(image).toHaveLength(2);
+  });
 });
