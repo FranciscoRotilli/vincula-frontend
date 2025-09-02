4b814c221ccedcf72ddd29e69a2ef7070012c1b2
Carolina Ferreira <ninaferreira2409@gmail.com>
2025-09-02 18:27:53 -0300

    fix: generic modal props

--- src/app/ModalShowcase/page.tsx
+++ src/app/ModalShowcase/page.tsx
@@ -50,7 +50,8 @@ export default function ShowcasePage() {
         onClose={function (): void {
           throw new Error('Function not implemented.');
         }}
-        buttons={['Cancelar', 'Salvar']}
+        actionButton="Salvar"
+        cancelButton="Cancelar"
       >
         <div className={showcaseStyles.formStack}>
           <label>Nome</label>
--- src/components/modalGenerico/Modal.tsx
+++ src/components/modalGenerico/Modal.tsx
@@ -8,7 +8,8 @@ type ModalProps = {
   children: ReactNode;
   size?: 'small' | 'medium' | 'large' | 'fullscreen';
   closeOnOverlayClick?: boolean;
-  buttons?: string[];
+  actionButton?: string;
+  cancelButton?: string;
   disableEscapeKeyDown?: boolean;
   onAction?: () => void;
   isError?: boolean;
@@ -22,8 +23,9 @@ const Modal = ({
   size = 'medium',
   closeOnOverlayClick = true,
   disableEscapeKeyDown = false,
-  buttons,
   onAction,
+  actionButton,
+  cancelButton,
   isError = false,
 }: ModalProps) => {
   useEffect(() => {
@@ -74,17 +76,24 @@ const Modal = ({
 
         <main className={styles.content}>{children}</main>
 
-        {buttons && (
+        {(cancelButton || actionButton) && (
           <footer className={styles.footer}>
-            <button className={isError ? `${styles.button}` : `${styles.button} ${styles.secondary}`} onClick={onClose}>
-              {buttons[0]}
-            </button>
-            <button
-              className={isError ? `${styles.button} ${styles.error}` : `${styles.button} ${styles.primary}`}
-              onClick={onAction}
-            >
-              {buttons[1]}
-            </button>
+            {cancelButton && (
+              <button
+                className={isError ? `${styles.button}` : `${styles.button}  ${styles.secondary}`}
+                onClick={onClose}
+              >
+                {cancelButton}
+              </button>
+            )}
+            {actionButton && (
+              <button
+                className={isError ? `${styles.button} ${styles.error}` : `${styles.button} ${styles.primary}`}
+                onClick={onAction}
+              >
+                {actionButton}
+              </button>
+            )}
           </footer>
         )}
       </div>
