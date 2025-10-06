50e7147f8cb7bff89d99c56eea0fe29c1de02ea3
lkunrath <lkunrath04@gmail.com>
2025-10-05 21:22:03 -0300

    feat: adicionar textos para filtros e visualização de dados

--- src/texts.ts
+++ src/texts.ts
@@ -76,6 +76,28 @@ export const texts = {
   'files.description':
     'Os arquivos em anexo serão usados para a geração de vínculos com os investigados.',
 
+  'filter.label1': 'Investigado',
+  'filter.label2': 'CPF/CNPJ',
+  'filter.label3': 'Destino',
+  'filter.label4': 'Base de dados',
+  'filter.apply': 'Aplicar Filtros',
+  'filter.clear': 'Limpar Filtros',
+
+  'filter.placeholder1': 'Selecione',
+  'filter.placeholder2': 'Digite o CPF/CNPJ',
+  'filter.placeholder3': 'Digite o CPF/CNPJ de destino',
+
+  'visualization.filters': 'Filtros',
+  'visualization.filtersDesc': 'Clique em um filtro para aplicá-lo',
+  'visualization.availableFiles': 'Arquivos Disponíveis ({count})',
+  'visualization.availableFilesDesc': 'Clique em um arquivo para carregar seus dados para a tabela',
+  'visualization.noFiles': 'Nenhum arquivo disponível.',
+  'visualization.noData': 'Nenhum dado encontrado.',
+  'visualization.loading': '...',
+  'visualization.clearButton': 'Limpar',
+  'visualization.filesLoadError': 'Falha ao carregar arquivos.',
+  'visualization.dataLoadError': 'Falha ao carregar dados do arquivo.',
+
   'footer.copy': '© Ministério Público do Estado do Rio Grande do Sul',
   'cases.title.changeNameDesc': 'Altere o nome do caso abaixo.',
   'cases.title.changeSituationDesc': 'Selecione a nova situação do caso.',
