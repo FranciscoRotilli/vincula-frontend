80541168373bdd198088f2cd1bcb537179c84ac0
Guilherme <quidoguim@icloud.com>
2025-10-21 14:48:36 -0300

    fix: remove duplicate import of ArrowDropDownIcon in MultiSelectDropdown component

--- src/components/MultiSelectDropdown/index.tsx
+++ src/components/MultiSelectDropdown/index.tsx
@@ -1,9 +1,8 @@
 'use client';
 
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import CheckBoxOutlineBlankRoundedIcon from '@mui/icons-material/CheckBoxOutlineBlankRounded';
 import CheckBoxRoundedIcon from '@mui/icons-material/CheckBoxRounded';
-//import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import React, { useEffect, useMemo, useState } from 'react';
 
 import styles from './MultiSelectDropdown.module.css';
