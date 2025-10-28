89e2a4dd0634a3062b49875dc132a3044cb4a5f3
Flavia Tavaniello <flatavaniello@gmail.com>
2025-10-27 22:14:00 -0300

    fix: existing unit tests fixed and all pass

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,17 +1,24 @@
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vitest/config';
+import path from 'node:path'
+
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vitest/config'
+
 export default defineConfig({
   plugins: [react()],
   test: {
     environment: 'jsdom',
     setupFiles: ['test/setupTests.ts'],
     include: ['test/**/*.{test,spec}.{ts,tsx}', 'src/**/*.{test,spec}.{ts,tsx}'],
-    css: true,
     globals: true,
+    fileParallelism: false,
   },
+  css: false,
+
   resolve: {
-    alias: {
-      '@': '/src',
-    },
+    alias: [
+      { find: '@', replacement: path.resolve(__dirname, 'src') },
+      { find: /^@mui\/icons-material\/.*/, replacement: path.resolve(__dirname, 'test/__mocks__/mui-icon-one.tsx') },
+      { find: '@mui/icons-material', replacement: path.resolve(__dirname, 'test/__mocks__/mui-icons-barrel.tsx') },
+    ],
   },
 });
